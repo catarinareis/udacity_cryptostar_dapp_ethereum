961a5c43b2fab4eb55051bd98684fb0a87265781
catarinareis <catarina.isabel.reis@gmail.com>
2020-01-21 22:40:27 +0000

    Implement the function that transfers a star from the address of the caller to a specific recipient address

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -80,7 +80,9 @@ contract StarNotary is ERC721 {
     // Implement Task 1 Transfer Stars
     function transferStar(address _to1, uint256 _tokenId) public {
         //1. Check if the sender is the ownerOf(_tokenId)
+        require(ownerOf(_tokenId) == msg.sender, "You can't exchange stars that you don't own");
         //2. Use the transferFrom(from, to, tokenId); function to transfer the Star
+        _transferFrom(msg.sender, _to1, _tokenId);
     }
 
 }
