74d33627d930656ed5c2afabca334001ed618ac6
catarinareis <catarina.isabel.reis@gmail.com>
2020-01-22 00:50:15 +0000

    Task 4 - Modify the front end of the DAPP to Lookup a star by ID and present her name

--- app/src/index.js
+++ app/src/index.js
@@ -41,7 +41,10 @@ const App = {
 
   // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
-    
+    const { lookUptokenIdToStarInfo } = this.meta.methods;
+    const tokenId = document.getElementById("lookid").value;
+    const starName = await lookUptokenIdToStarInfo(tokenId).call();
+    App.setStatus("The Star Name is " + starName + ".");   
   }
 
 };
